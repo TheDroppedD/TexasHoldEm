ac697b7a8a686535ad4fa4537139dc1c0b6cf9bb
TheDroppedD <alvie_hossain@yahoo.com>
2017-09-16 00:41:52 -0700

    Constructor for Card
    Card Constructor made

--- Card.cs
+++ Card.cs
@@ -9,5 +9,10 @@ namespace TexasHoldEm
          Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, 
          King = 13, Ace = 14}
          */
+
+         public Card(string suit, string rank){
+             this.suit = suit;
+             this.rank = rank; 
+         }
     }
 }
