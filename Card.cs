6a5fe81ed2b068331ac8db80d57f14bd34fa3438
TheDroppedD <alvie_hossain@yahoo.com>
2017-09-16 00:30:56 -0700

    Card Class Fields Updated
    TItle is explanatory, not much else

--- Card.cs
+++ Card.cs
@@ -4,8 +4,10 @@ namespace TexasHoldEm
 {
     class Card{
         private string suit;
-        private enum values {Two = 2, Three = 3, Four = 4, Five = 5, Six = 6,
+        private string rank;
+        /*private enum values {Two = 2, Three = 3, Four = 4, Five = 5, Six = 6,
          Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, 
          King = 13, Ace = 14}
+         */
     }
 }
