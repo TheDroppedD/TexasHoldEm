15d49705bc2a5c68ce5a00a0df9e1819adfa33be
Gojeflone <avak1908@gmail.com>
2017-09-16 04:03:53 -0400

    Made RankComparer
    Rank Comparer will be used in Collections.sort method

--- CustomCardRankComparer.cs
+++ CustomCardRankComparer.cs
@@ -2,8 +2,19 @@ using System;
 using System.Collections;
 namespace TexasHoldEm {
 
-class CustomerCardRankComparer : IComparer<Card> {
-    private readonly IComparer;
+class CustomerCardRankComparer : IComparer {
+    int IComparer.Compare(Card x, Card y) {
+        Card c1 = (Card)x;
+        Card c2 = (Card)y;
+        //getRankValue
+        if(c1.getRankValue()  < c2.getRankValue()) {
+            return -1;
+        } else if(c1.getRankValue() > c2.getRankValue()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
 
 }
