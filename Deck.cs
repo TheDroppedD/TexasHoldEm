e47b2170994cd910787050cb11d682f631781b19
Gojeflone <avak1908@gmail.com>
2017-09-16 00:42:58 -0400

    Added Deck and Driver
    Deck is class, Driver holds main function

---
+++ Deck.cs
@@ -0,0 +1,13 @@
+using System;
+namespace TexasHoldEm
+
+{
+    class Deck{
+        private string suit;
+        private string rank;
+        /*private enum values {Two = 2, Three = 3, Four = 4, Five = 5, Six = 6,
+         Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, 
+         King = 13, Ace = 14}
+         */
+    }
+}
---
+++ PokerDriver.cs
@@ -0,0 +1,8 @@
+using System;
+namespace TexasHoldEm {
+    class PokerDriver {
+        static void Main(string[] args) {
+            Console.WriteLine("This is WORKING!");
+        }
+    }
+}
