368679e99234bc0b1da83728518931934ed85071
TheDroppedD <alvie_hossain@yahoo.com>
2017-09-16 00:22:07 -0700

    Card Class Started
    Currently figuring out how to use the cards

---
+++ Card.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TexasHoldEm
+
+{
+    class Card{
+        private string suit;
+        private enum values {Two = 2, Three = 3, Four = 4, Five = 5, Six = 6,
+         Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, 
+         King = 13, Ace = 14}
+    }
+}
