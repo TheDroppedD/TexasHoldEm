38dbead2c02bbd5efa324bb99ffe6a54b1f8652f
Gojeflone <avak1908@gmail.com>
2017-09-15 23:55:55 -0400

    Made Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TexasHoldEm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SecondFile.cs
@@ -0,0 +1,15 @@
+using System;
+namespace HelloWorldApp
+{
+    /* 
+    class HelloWorld
+    {
+        static void Main() {
+            //Some shit 
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+
+    */
+}
