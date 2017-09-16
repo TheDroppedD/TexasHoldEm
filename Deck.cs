d743123dda7667e55cb6ebed074040e36c972d22
Gojeflone <avak1908@gmail.com>
2017-09-16 07:51:03 -0400

    Changes to deck.cs

--- Deck.cs
+++ Deck.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 namespace TexasHoldEm
 {
     class Deck{
+        private readonly int deckSize  = 52;
         private List<Card> theDeck = new List<Card>();
+        
         /*private enum values {Two = 2, Three, Four, Five, Six,
         Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
         */
@@ -28,6 +30,23 @@ namespace TexasHoldEm
             theDeck.RemoveAt(0);
             //Console.WriteLine(c1.ToString()); //For testing
             return c1;
+        }
+
+        //Deck needs shuffle method
+        public void shuffleDeck() {
+            //shuffles shit,
+            /* 
+            List<Card> newDeck = new List<Card>(deckSize);
+            Random rnd = new Random();
+            int i = 0;
+            foreach(Card c in theDeck) {
+            newDeck[i] = theDeck[rnd.Next(0,52)];
+            int i =0
+            */
+            var rnd = new Random();
+            var result = source.OrderBy(item => rnd.Next());
+            }
+
         }
 }
 }
